9af0718b3e46d994492243a563e0de0e00b18c26
RedWolfie <arui939@gmail.com>
2023-08-18 18:07:37 +0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define a route and a function to handle it
+@app.route('/')
+def index():
+    return render_template("index.html")
+a
+
+# Run the app if this script is executed directly
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
