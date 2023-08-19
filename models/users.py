0dae4e7b04df7faafab138d7992474edfd9f5054
RedWolfie <arui939@gmail.com>
2023-08-19 12:19:36 +0300

    :+1: added login/register functions and added password encryption

--- models/users.py
+++ models/users.py
@@ -1,11 +1,15 @@
+from flask_bcrypt import Bcrypt
 from sqlalchemy.sql import func
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 class User(db.Model):
+    # General Website details
     id = db.Column(db.BigInteger, primary_key=True)
     siteUsername = db.Column(db.String(512), unique=True, nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)  # Rename to password_hash
     gameUsername = db.Column(db.String(512), unique=False, nullable=False)
     email = db.Column(db.String(512), unique=True, nullable=False)
     timestamp = db.Column(db.DateTime(timezone=True),
@@ -15,7 +19,19 @@ class User(db.Model):
     playerId = db.Column(db.BigInteger, unique=True, nullable=False)
     accountId = db.Column(db.BigInteger, unique=True, nullable=False)
 
+    # Patreon sub Tier
     tier = db.Column(db.Integer, unique=False, nullable=False)
-    
+
+    # Settings
+    port = db.Column(db.String, unique=False, nullable=False)
+    serverIp = db.Column(db.String, unique=True, nullable=False)
+    renderCustomIcons = db.Column(db.String, unique=False, nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
     def __repr__(self):
-        return f"<User {self.username}>"
+        return f"<User {self.siteUsername}>"
--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 import os
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
+from flask import Flask, render_template, request, redirect, url_for, session
+from models.users import db, User
 
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 from models.users import db as user_db
 
@@ -15,9 +11,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+        'sqlite:///' + os.path.join(basedir, 'users.db')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+with app.app_context():
+    user_db.init_app(app)
+    user_db.create_all()
 
 # Define a route and a function to handle it
 @app.route('/')
@@ -28,9 +28,48 @@ def index():
 def privacy():
     return render_template("Privacy.html")
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.password == password:
+            # Store user session data (you might want to use Flask-Login for session management)
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        else:
+            error = "Invalid credentials"
+            return render_template("login.html", error=error)
+
+    return render_template("login.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if password == confirm_password:
+            # Create a new User instance and add it to the database
+            new_user = User(siteUsername=username, email=email)
+            # Store the hashed password securely (you would use proper password hashing here)
+            new_user.password = password
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login'))  # Redirect to log in after successful registration
+        else:
+            error = "Passwords do not match"
+            return render_template("register.html", error=error)
+
+    return render_template("register.html")
+
 
 # Run the app if this script is executed directly
 if __name__ == '__main__':
-    user_db.init_app(app)
     app.run(debug=True, host="0.0.0.0")
     
